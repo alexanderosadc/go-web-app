02745db52430ccef404032fd0a81584b22ecb26b
alexanderosadc <alexanderosadc@gmail.com>
2023-03-18 22:22:41 +0200

    feat: Adds session middleware which stores and loads session data on each request

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -6,13 +6,19 @@ import (
 	"github.com/justinas/nosurf"
 )
 
+// NoSurf adds CSRF protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// SessionLoad loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return app.Session.LoadAndSave(next)
 }
