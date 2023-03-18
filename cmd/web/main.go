f08f7b30b1d68b4a43d1097fe2256b5ff9cc1380
alexanderosadc <alexanderosadc@gmail.com>
2023-03-18 20:33:05 +0200

    feat: Creates config file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/alexanderosadc/go-web-app/pkg/config"
 	"github.com/alexanderosadc/go-web-app/pkg/handlers"
+	"github.com/alexanderosadc/go-web-app/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
