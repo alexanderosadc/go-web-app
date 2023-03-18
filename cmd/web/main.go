e73b1ef25f0ea6d3eac195d463987a5a8e854340
alexanderosadc <alexanderosadc@gmail.com>
2023-03-18 22:22:11 +0200

    feat: Adds session which adds persistance connection to the web app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/alexanderosadc/go-web-app/pkg/config"
 	"github.com/alexanderosadc/go-web-app/pkg/handlers"
 	"github.com/alexanderosadc/go-web-app/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+
 func main() {
-	var app config.AppConfig
+
+	app.InProduction = false
+
+	session := scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
